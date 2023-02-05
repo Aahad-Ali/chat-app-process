ff605efb0060fcc953ad0b2174eb8e9aea08d7e2
Aahad Ali <104500845+Aahad-Ali@users.noreply.github.com>
2023-02-05 17:41:32 +0500

    Add files via upload

--- web/src/components/userList.jsx
+++ web/src/components/userList.jsx
@@ -6,36 +6,29 @@ import { GlobalContext } from "./../context/Context";
 import moment from "moment";
 
 function Home() {
-  const [searchTerm, setSearchTerm] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
   const [users, setUsers] = useState([]);
 
   let { state, dispatch } = useContext(GlobalContext);
 
   useEffect(() => {}, []);
 
-  const getUsers =async()=>{
-
+  const getUsers = async () => {
     try {
-      const response = await axios.get(`${state.baseUrl}/users?q=${searchTerm}`)
+      const response = await axios.get(
+        `${state.baseUrl}/users?q=${searchTerm}`
+      );
       console.log("response: ", response.data);
 
-      if (response.data.data.length === 0) setEof(true);
+      setUsers(response.data);
 
-
-
-      setTweets((prev) => {
-
-          // if (prev.length >= 10) {
-          //     prev = prev.slice(5)
-          // }
-          return [...prev, ...response.data.data]
-      })
-
-  } catch (error) {
+      
+    } catch (error) {
       console.log("error in getting all tweets", error);
-  }
+      // setUsers([]);
 
-  }
+    }
+  };
 
   return (
     <div>
@@ -56,6 +49,9 @@ function Home() {
           <span>{eachUser.email}</span>
         </div>;
       })}
+      {(users.length === 0? 'No User Found':null)}
+      {(users === null ? 'Loading...':null)}
+
     </div>
   );
 }

99c9cf6e3638f752b052314cffe37faa16c819bd
Aahad Ali <104500845+Aahad-Ali@users.noreply.github.com>
2023-03-21 18:57:16 +0000

    chat app complete in localhost

--- web/src/components/chatScreen.jsx
+++ web/src/components/chatScreen.jsx
@@ -1,153 +1,133 @@
-import { useFormik } from 'formik';
-import * as yup from 'yup';
+import { useFormik } from "formik";
+import * as yup from "yup";
 import axios from "axios";
-import { useEffect, useState, useContext } from 'react';
-import { GlobalContext } from './../context/Context';
-import moment from 'moment';
-import InfiniteScroll from 'react-infinite-scroller';
+import { useEffect, useState, useContext } from "react";
+import { GlobalContext } from "./../context/Context";
+import moment from "moment";
+import InfiniteScroll from "react-infinite-scroller";
 import { useParams } from "react-router-dom";
 import { io } from "socket.io-client";
 // import './App.css';
 
-
-
-
-
 function ChatScreen() {
+  let { state, dispatch } = useContext(GlobalContext);
+  const { id } = useParams();
 
-    let { state, dispatch } = useContext(GlobalContext);
-    const { id } = useParams();
+  const [writeMessage, setWriteMessage] = useState("");
+  const [conversation, setConversation] = useState(null);
+  const [recipientProfile, setRecipientProfile] = useState({});
 
-
-    const [writeMessage, setWriteMessage] = useState("");
-    const [conversation, setConversation] = useState(null);
-    const [recipientProfile, setRecipientProfile] = useState({});
-
-    const getMessages = async () => {
-        try {
-            const response = await axios.get(`${state.baseUrl}/messages/${id}`)
-            console.log("response: ", response.data);
-            setConversation(response.data)
-
-        } catch (error) {
-            console.log("error in getting all tweets", error);
-        }
+  const getMessages = async () => {
+    try {
+      const response = await axios.get(`${state.baseUrl}/messages/${id}`);
+      console.log("response: ", response.data);
+      setConversation(response.data);
+    } catch (error) {
+      console.log("error in getting all tweets", error);
     }
-    const getRecipientProfile = async () => {
-        try {
-            let response = await axios.get(
-                `${state.baseUrl}/profile/${id}`,
-                {
-                    withCredentials: true
-                });
+  };
+  const getRecipientProfile = async () => {
+    try {
+      let response = await axios.get(`${state.baseUrl}/profile/${id}`, {
+        withCredentials: true,
+      });
 
-            console.log("RecipientProfile:", response);
-            setRecipientProfile(response.data)
-        } catch (error) {
-            console.log("axios error: ", error);
-        }
+      console.log("RecipientProfile:", response);
+      setRecipientProfile(response.data);
+    } catch (error) {
+      console.log("axios error: ", error);
     }
+  };
 
+  useEffect(() => {
+    const socket = io(state.baseUrlSocketIo, {
+      withCredentials: true,
+    });
 
-    useEffect(() => {
+    socket.on("connect", function () {
+      console.log("connected");
+    });
+    socket.on("disconnect", function (message) {
+      console.log("Socket disconnected from server: ", message);
+    });
+    socket.on("connect_error", (err) => {
+      console.log(`connect_error due to ${err.message}`);
+    });
 
-        const socket = io(state.baseUrlSocketIo, {
-            withCredentials: true
-        });
+    console.log("subscribed: ", `${state.user._id}-${id}`);
+    socket.on(`${state.user._id}-${id}`, function (data) {
+      console.log(data);
+      setConversation((prev) => [data, ...prev]);
+    });
 
-        socket.on('connect', function () {
-            console.log("connected")
-        });
-        socket.on('disconnect', function (message) {
-            console.log("Socket disconnected from server: ", message);
-        });
-        socket.on("connect_error", (err) => {
-            console.log(`connect_error due to ${err.message}`);
-        });
+    return () => {
+      socket.close();
+    };
+  }, []);
 
-        console.log("subscribed: ", `${state.user._id}-${id}`);
-        socket.on(`${state.user._id}-${id}`, function (data) {
+  useEffect(() => {
+    getRecipientProfile();
+    getMessages();
+  }, []);
 
-            console.log(data);
-            setConversation(prev => [data, ...prev])
-        });
+  const sendMessage = async (e) => {
+    if (e) e.preventDefault();
 
+    try {
+      const response = await axios.post(`${state.baseUrl}/message`, {
+        to: id,
+        text: writeMessage,
+      });
+      console.log("response: ", response.data);
+      getMessages();
+    } catch (error) {
+      console.log("error in getting all tweets", error);
+    }
+  };
 
-        return () => {
-            socket.close();
-        }
+  return (
+    <div className="chatScreen-main">
+      <h1>Chat with '{recipientProfile?.firstName}' </h1>
+      <span>'{recipientProfile?.email} '</span>
 
-    }, [])
+      <form onSubmit={sendMessage} className="chat-person">
+        <input
+          type="text"
+          placeholder="type your message"
+          onChange={(e) => [setWriteMessage(e.target.value)]}
+        />
+        <button type="submit">Send</button>
+      </form>
 
-    useEffect(() => {
+      <div className="messageView">
+        {conversation?.length
+          ? conversation?.map((eachMessage, index) => {
+              const className =
+                eachMessage.from._id === id ? "recipientMessage" : "myMessage";
 
-        getRecipientProfile();
-        getMessages();
+              return (
+                <div key={index} className={`message ${className}`}>
+                    <div className='name' >{eachMessage.from.firstName} </div>
 
-    }, [])
-
-
-
-
-
-
-    const sendMessage = async (e) => {
-        if (e) e.preventDefault();
-
-        try {
-            const response = await axios.post(`${state.baseUrl}/message`, {
-                to: id,
-                text: writeMessage,
+                  <div className="text">{eachMessage.text}</div>
+                  <div className="head">
+                    <br />
+                    <div className="time">
+                      {moment(eachMessage.createdOn).fromNow()}
+                    </div>
+                    {/* <div className='name' >{eachMessage.from.firstName} </div> */}
+                  </div>
+                </div>
+              );
+              // {eachMessage.from.lastName}
             })
-            console.log("response: ", response.data);
-            getMessages();
-        } catch (error) {
-            console.log("error in getting all tweets", error);
-        }
-    }
+          : null}
+      </div>
 
-
-    return (
-        <div className='chatScreen-main'>
-            <h1>Chat with '{recipientProfile?.firstName}' </h1>
-            <span>'{recipientProfile?.email} '</span>
-
-            <form onSubmit={sendMessage} className="chat-person">
-                <input type="text" placeholder='type your message' onChange={(e) => [
-                    setWriteMessage(e.target.value)
-                ]} />
-                <button type="submit">Send</button>
-            </form>
-
-            <div className='messageView'>
-
-                {(conversation?.length) ?
-                    conversation?.map((eachMessage, index) => {
-                        const className = (eachMessage.from._id === id) ? "recipientMessage" : "myMessage"
-
-                        return <div
-                        id='msg'
-                            key={index}
-                            className={`message ${className}`}>
-                            <div className='text' >{eachMessage.text}</div><br />
-                            <div className='head'>
-                                <div className='time' >{moment(eachMessage.createdOn).fromNow()}</div>
-                                {/* <div className='name' >{eachMessage.from.firstName} </div> */}
-                            </div>
-                        </div>
-                        // {eachMessage.from.lastName}
-
-                    })
-                    : null
-                }
-            </div>
-
-            {(conversation?.length === 0 ? "No Messages found" : null)}
-            {(conversation === null ? "Loading..." : null)}
-
-
-        </div>
-    );
+      {conversation?.length === 0 ? "No Messages found" : null}
+      {conversation === null ? "Loading..." : null}
+    </div>
+  );
 }
 
 export default ChatScreen;
